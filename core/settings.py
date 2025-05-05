3cce06cae5d8b361be79769d24e158ac07f113c4
UbongPr7 <ubongpr7@gmail.com>
2025-05-05 19:42:09 +0100

    fix: Update EMAIL_HOST and enable TLS for email configuration

--- core/settings.py
+++ core/settings.py
@@ -176,10 +176,10 @@ STORAGES = {
 }
 
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-EMAIL_HOST = 'mail.destinybuilders.africa'
+EMAIL_HOST = 'mails.destinybuilders.africa'
 EMAIL_PORT = 587  
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+EMAIL_USE_TLS= True
 EMAIL_HOST_USER =os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
