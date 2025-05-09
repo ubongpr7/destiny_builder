81627b8e628ecab9140e25ea27667d9b94d34bbb
UbongPr7 <ubongpr7@gmail.com>
2025-05-09 23:50:05 +0100

    fix: Add EMAIL_HOST_USER to settings.py for Gmail configuration

--- core/settings.py
+++ core/settings.py
@@ -198,6 +198,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465  
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
+EMAIL_HOST_USER = "ubongpr7@gmail.com"
 EMAIL_HOST_PASSWORD = "nmcmiwlgwdrwesef"
 
 
