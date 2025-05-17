d37b089f1cf8d0ff4b6473cdf9fc065d4b71fa40
UbongPr7 <ubongpr7@gmail.com>
2025-05-18 00:21:57 +0100

    task manaement system

--- mainapps/project_task/admin.py
+++ mainapps/project_task/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task, TaskComment, TaskAttachment, TaskTimeLog, TaskStatus, TaskPriority, TaskType
+
+
+admin.site.register(Task)
+admin.site.register(TaskComment)
+admin.site.register(TaskAttachment)
+admin.site.register(TaskTimeLog)
+admin.site.register(TaskStatus)
+admin.site.register(TaskPriority)
+admin.site.register(TaskType)
 
 # Register your models here.
