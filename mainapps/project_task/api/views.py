f588b066257a7a3979d873c2235a688b3a506304
UbongPr7 <ubongpr7@gmail.com>
2025-05-20 19:32:18 +0100

    task manaement system

--- mainapps/project_task/api/views.py
+++ mainapps/project_task/api/views.py
@@ -259,7 +259,6 @@ class TaskViewSet(viewsets.ModelViewSet):
         queryset = queryset.order_by(ordering)
         
         serializer = self.get_serializer(queryset, many=True)
-        print(serializer.data)
         return Response(serializer.data)
 
     # @action(detail=False, methods=['get'])
