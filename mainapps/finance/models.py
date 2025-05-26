18b93834dbf5392e1e5d6dd49964c01fc42afdcf
UbongPr7 <ubongpr7@gmail.com>
2025-05-26 19:21:53 +0100

    Finance and budgeting app

--- mainapps/finance/models.py
+++ mainapps/finance/models.py
@@ -240,7 +240,9 @@ class BankAccount(models.Model):
     created_by = models.ForeignKey(
         User,
         on_delete=models.PROTECT,
-        related_name='created_accounts'
+        related_name='created_accounts',
+        blank=True,
+        null=True,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
