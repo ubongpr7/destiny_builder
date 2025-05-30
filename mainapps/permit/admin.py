681ff7ebe986259ca439db5b5e372b33fe179718
UbongPr7 <ubongpr7@gmail.com>
2025-05-30 10:18:25 +0100

    Activity logging

--- mainapps/permit/admin.py
+++ mainapps/permit/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PermissionCategory,CustomUserPermission
+from .models import PermissionCategory,CustomUserPermission,ActivityLog
 
 admin.site.register(PermissionCategory)
 admin.site.register(CustomUserPermission)
+admin.site.register(ActivityLog)
