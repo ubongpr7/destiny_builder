5f88bcdce1580419265bb69e19aeef11a7a4c60a
UbongPr7 <ubongpr7@gmail.com>
2025-06-02 00:47:38 +0100

    Flutterwave

--- mainapps/finance/urls.py
+++ mainapps/finance/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import FlutterwaveWebhookView
+from .views import FlutterwaveWebhookView,flutterwave_webhook_debug
 urlpatterns = [
-    path('test_webhook/', FlutterwaveWebhookView.as_view(), name='flutterwave_webhook'),
+    path('test_webhook/', flutterwave_webhook_debug, name='flutterwave_webhook'),
+    # path('test_webhook/', FlutterwaveWebhookView.as_view(), name='flutterwave_webhook'),
 ]
